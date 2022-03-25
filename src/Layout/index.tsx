2eb1a942d8acd4f5146b3f8a3d3653747d6f1f0b
HYEOK_E <jhjeong00@gmail.com>
2022-03-26 02:22:44 +0900

    env: project setting

---
+++ src/Layout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <Routes>
+      <Route />
+    </Routes>
+  );
+};
+
+export default Layout;
